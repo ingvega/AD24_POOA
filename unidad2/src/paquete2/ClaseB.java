0b9dee047e360c4adf1e20ad00a2d346a79132f4
ingvega <isc.p.vega@gmail.com>
2024-08-22 16:07:56 -0600

    Ejemplo de uso de paquetes y modificadores de acceso

---
+++ unidad2/src/paquete2/ClaseB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete2;
+
+/**
+ *
+ * @author paveg
+ */
+public class ClaseB {
+    String nombre;
+}
